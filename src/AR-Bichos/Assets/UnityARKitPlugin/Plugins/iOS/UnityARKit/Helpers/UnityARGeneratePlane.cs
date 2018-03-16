d23f53267ebf04d2608855ed7f4a820a383809ff
pontura <pontura@gmail.com>
2018-03-16 11:26:55 -0300

    ui

--- src/AR-Bichos/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARGeneratePlane.cs
+++ src/AR-Bichos/Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/Helpers/UnityARGeneratePlane.cs
@@ -25,6 +25,7 @@ namespace UnityEngine.XR.iOS
             List<ARPlaneAnchorGameObject> arpags = unityARAnchorManager.GetCurrentPlaneAnchors ();
             if (arpags.Count >= 1) {
 				UIPanels.Instance.configScreens.SetState (ConfigScreens.states.TOUCH_TO_CREATE);
+				return;
                 //ARPlaneAnchor ap = arpags [0].planeAnchor;
                 //GUI.Box (new Rect (100, 100, 800, 60), string.Format ("Center: x:{0}, y:{1}, z:{2}", ap.center.x, ap.center.y, ap.center.z));
                 //GUI.Box(new Rect(100, 200, 800, 60), string.Format ("Extent: x:{0}, y:{1}, z:{2}", ap.extent.x, ap.extent.y, ap.extent.z));
